3220bc09f764aa031bcb94e9f47188645b22770e
wempl-git <116441757+Wempl@users.noreply.github.com>
2024-02-24 12:40:43 +0300

    Update ООП in python.py

--- classes/ООП in python.py
+++ classes/ООП in python.py
@@ -1,25 +1,25 @@
 #       -----------------------------------------
 #   Создание класса в python: 
 #   Пример: создание класса с целью описания котов
-class Cat:
-    name = None
-    age = None
-    isHappy = None
+# class Cat:
+#     name = None
+#     age = None
+#     isHappy = None
 
-    def set_data(self, name, age, isHappy):
-        self.name = name
-        self.age = age
-        self.isHappy = isHappy
+#     def set_data(self, name, age, isHappy):
+#         self.name = name
+#         self.age = age
+#         self.isHappy = isHappy
 
-    def get_data(self):
-        print(self.name, 'age:', self.age, '. Happy:', self.isHappy)
+#     def get_data(self):
+#         print(self.name, 'age:', self.age, '. Happy:', self.isHappy)
 
-cat1 = Cat()
-cat1.set_data('Барсик', 3, True)
+# cat1 = Cat()
+# cat1.set_data('Барсик', 3, True)
 
-cat2 = Cat()
-cat2.set_data('Васька', 2, False)
+# cat2 = Cat()
+# cat2.set_data('Васька', 2, False)
 
-cat1.get_data()
-cat2.get_data()
+# cat1.get_data()
+# cat2.get_data()
 #       -----------------------------------------
