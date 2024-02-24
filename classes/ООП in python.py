049ba3561033c28cc31d8e0bd4d20d9c7b2788c4
wempl-git <116441757+Wempl@users.noreply.github.com>
2024-02-24 15:50:25 +0300

    Update ООП in python.py

--- classes/ООП in python.py
+++ classes/ООП in python.py
@@ -23,3 +23,28 @@
 # cat1.get_data()
 # cat2.get_data()
 #       -----------------------------------------
+
+#       -----------------------------------------
+#   Создание конструкторов
+# class Cat:
+#     name = None
+#     age = None
+#     isHappy = None
+
+#     def __init__(self, name = 'Неизвестно', age = 'Неизвестен', isHappy = 'Неизвестно'):
+#         self.set_data(name, age, isHappy)
+#         self.get_data()
+
+#     def set_data(self, name = 'Неизвестно', age = 'Неизвестен', isHappy = 'Неизвестно'):
+#         self.name = name
+#         self.age = age
+#         self.isHappy = isHappy
+
+#     def get_data(self):
+#         print(self.name, 'age:', self.age, '. Happy:', self.isHappy)
+
+# cat1 = Cat('Барсик', True)
+# cat1.set_data('Джон', 2)
+
+# cat2 = Cat('Васька', 2, False)
+#       -----------------------------------------
