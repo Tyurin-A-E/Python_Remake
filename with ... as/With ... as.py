dbb9484acfe25abbaeaf4da7ddb1c162201a29bd
wempl-git <116441757+Wempl@users.noreply.github.com>
2024-02-24 12:41:36 +0300

    Create With ... as.py

---
+++ with ... as/With ... as.py
@@ -0,0 +1,9 @@
+#       -----------------------------------------
+#   Базовая конструкция With ... as:
+#   P.s. Файл заведомо не создан для получения ошибки
+# try:
+#     with open('text.txt', 'r', encoding='utf-8') as file:
+#         file.read()
+# except FileNotFoundError:
+#     print("File not found. Please update the file folder")
+#       -----------------------------------------
